e8488828461cecdfb6830e9965462a4a60f7ee32
kviet91 <kviet91.cdp@gmail.com>
2020-11-25 14:23:06 +0700

    De_quy_menu_2cap

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,20 @@ use App\Models\Menu;
 class HomeController extends Controller
 {
     public function index() {
+        $menus0 = Menu::where('type','header_main')->orderby('order')->get()->toArray();
+        $arr_menu = [];
+        
+        foreach ($menus0 as $menu0) {
+            $arr_menu[$menu0['parent_id']][] = $menu0;
+        }
+        // dd($arr_menu);
+
+        view()->share('menus0', $menus0);
+        view()->share('arr_menu', $arr_menu);
+
+
+
+
         return view('pages.index');
         
     }
